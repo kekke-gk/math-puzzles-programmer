e622fa78bd0762970bd094b60b53ed9827412d81
kekke-gk <gk0509hn257@gmail.com>
2024-06-07 01:15:04 +0900

    Refactor Q03

--- q03.hs
+++ q03.hs
@@ -1,10 +1,4 @@
-flipCards cards n
-    | n > len = cards
-    | otherwise = flipCards flipped (n+1)
-    where len = length cards
-          flags = take len $ cycle $ replicate n False ++ [True]
-          flipped = zipWith (\card flag -> if flag then not card else card) cards flags
-
 main = do
-    let cards = replicate 100 False
-    print $ [v | (v, flag) <- zip [1..] $ flipCards cards 1, not flag]
+    let n = 100
+    print $ map fst $ filter (even . snd) $ zip [1 ..] $ take n $
+        foldr1 (zipWith (+)) $ map (\n -> cycle $ replicate n 0 ++ [1]) [1 .. n]
