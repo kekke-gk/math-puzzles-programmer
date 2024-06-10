64b952ab7f0e02a7c0b9180e213bbf8c6ab223ed
kekke-gk <gk0509hn257@gmail.com>
2024-06-10 14:43:59 +0900

    Refactor Q19

--- q19.hs
+++ q19.hs
@@ -1,7 +1,6 @@
 import Data.List
 primes = [2, 3, 5, 7, 11, 13]
 
--- maxProd [a,b,c,d,e,f] = maximum [a*a,a*b,b*c,c*d,d*e,e*f,f*f]
 maxProd [a,b,c,d,e,f] = maximum [a*a,a*b,b*c,c*d,d*e,e*f,f*f]
 
 main = do
