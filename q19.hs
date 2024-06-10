724a7f5b58daec95be16fd1682a274dda55ac340
kekke-gk <gk0509hn257@gmail.com>
2024-06-10 14:14:44 +0900

    Solved Q19

---
+++ q19.hs
@@ -0,0 +1,8 @@
+import Data.List
+primes = [2, 3, 5, 7, 11, 13]
+
+-- maxProd [a,b,c,d,e,f] = maximum [a*a,a*b,b*c,c*d,d*e,e*f,f*f]
+maxProd [a,b,c,d,e,f] = maximum [a*a,a*b,b*c,c*d,d*e,e*f,f*f]
+
+main = do
+    print $ minimum $ map maxProd $ permutations primes
