277c5a9fbb5eeccb2629790951b5071f9413a0c7
kekke-gk <gk0509hn257@gmail.com>
2024-06-13 17:48:21 +0900

    Refactor Q27

--- q27.hs
+++ q27.hs
@@ -11,11 +11,6 @@ data Path = Path {
 instance Eq Path where
     Path s e == Path s' e' = (s == s' && e == e') || (s == e' && s' == e)
 
--- instance Ord Path where
---     compare (Path s e) (Path s' e')
---         | (s == s' && e == e') || (s == e' && s' == e) = EQ
---         | otherwise = compare s s' <> compare e e'
-
 unitShifts = map (uncurry Shift) [(1, 0), (0, 1), (-1, 0), (0, -1)]
 
 add a b = Shift (x a + x b) (y a + y b)
@@ -34,34 +29,7 @@ isTurnLeftOrStraight s s' =
         d' = dir s'
     in ((d + 1) `mod` 4) == d' || d == d'
 
--- isTurnRight s s' = dir s == ((dir s' + 1) `mod` 4)
 isTurnRight s s' = not $ isTurnLeftOrStraight s s'
-
-shifts2paths = scanl (\acc s -> Path (end acc) (add (end acc) s)) $ Path (Shift 0 0) (Shift 0 0)
-
--- move w h shifts
---     | x cur < 0 = 0
---     | y cur < 0 = 0
---     | x cur > w = 0
---     | y cur > h = 0
---     | length shifts >= 2 && isTurnRight (shifts !! 1) (shifts !! 0) = 0
---     | length shifts >= 2 && tookSamePath (shifts2paths shifts) = 0
---     | cur == Shift w h = 1
---     | otherwise = sum $ map (move w h . (:shifts)) unitShifts
---     where cur = foldl add (Shift 0 0) shifts
---     -- where cur = foldl1 add shifts
-
--- move w h shifts
---     | x cur < 0 = []
---     | y cur < 0 = []
---     | x cur > w = []
---     | y cur > h = []
---     | length shifts >= 2 && isTurnRight (shifts !! 1) (shifts !! 0) = []
---     | length shifts >= 2 && tookSamePath (shifts2paths shifts) = []
---     | cur == Shift w h = [shifts]
---     | otherwise = foldl1 (++) $ map (move w h . (:shifts)) unitShifts
---     -- where cur = foldl add (Shift 0 0) shifts
---     where cur = foldl1 add shifts
 
 move w h shifts paths
     | x cur < 0 = []
@@ -70,7 +38,7 @@ move w h shifts paths
     | y cur > h = []
     | length shifts >= 2 && isTurnRight (shifts !! 1) (shifts !! 0) = []
     | cur == Shift w h = [shifts]
-    | otherwise = foldl1 (++) [move w h (s:shifts) (newPath:paths) | s <- unitShifts, let newPath = Path cur (add cur s), newPath `notElem` paths]
+    | otherwise = foldl1 (++) [move w h (s:shifts) (p:paths) | s <- unitShifts, let p = Path cur (add cur s), p `notElem` paths]
     where cur = foldl1 add shifts
 
 main = do
